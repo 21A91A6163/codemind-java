b86dfd2c365f66e6d03eee2049a0ce54f647d8ee
21A91A6163 <97424651+21A91A6163@users.noreply.github.com>
2022-09-22 13:50:37 +0530

    import java.util.Scanner; class Find {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int min,i,c=0;         int[] arr=new int[100];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         min=arr[0];         for(i=0;i<n;i++)         {
                if(min>arr[i])
                {
                    min=arr[i];
                }
            }
            System.out.print(min);
        }
    }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Find
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int min,i,c=0;
+        int[] arr=new int[100];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        min=arr[0];
+        for(i=0;i<n;i++)
+        {
+            
+            if(min>arr[i])
+            {
+                min=arr[i];
+            }
+        }
+        System.out.print(min);
+    }
+}
