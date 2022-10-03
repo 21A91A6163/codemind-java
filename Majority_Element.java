8b24647daaf7ad83943473ec7bf16a7fccf11869
21A91A6163 <97424651+21A91A6163@users.noreply.github.com>
2022-10-03 15:05:30 +0530

    import java.util.Scanner; class Majority {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j,c=0;         n=sc.nextInt();         int arr[]=new int[100];         for(i=0;i<n;i++)             arr[i]=sc.nextInt();         for(i=0;i<n;i++)         {             c=0;             for(j=i;j<n;j++)             {                 if(arr[j]==arr[i])                 {                     c++;                 }             }             if(c>n/2)             {                 System.out.println(arr[i]);                 arr[i]=-1;                 break;             }         }     } }

---
+++ Majority_Element.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Majority
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j,c=0;
+        n=sc.nextInt();
+        int arr[]=new int[100];
+        for(i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            c=0;
+            for(j=i;j<n;j++)
+            {
+                if(arr[j]==arr[i])
+                {
+                    c++;
+                }
+            }
+            if(c>n/2)
+            {
+                System.out.println(arr[i]);
+                arr[i]=-1;
+                break;
+            }
+        }
+    }
+}
